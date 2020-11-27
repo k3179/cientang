ee31884b9e90622f8bbcbe1287c8e6e7fda3bb70
k3179 <yunkuangsu@gmail.com>
2020-11-27 10:22:55 +0800

    Initial Commit

---
+++ pages/user_login/user_login.js
@@ -0,0 +1,93 @@
+// pages/user_login/user_login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    username: '',
+    password: ''
+  },
+
+
+  // 获取输入账号 
+  usernameInput: function (e) {
+    this.setData({
+      username: e.detail.value
+    })
+  },
+ 
+  // 获取输入密码 
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+
+  formSubmit(e) {
+    wx.request({
+      url:'http://localhost:1405/app/user/login?userName=aaa&userPassword=bbb',
+      method: 'POST',
+      headers:{'Content-Type': 'application/json'},
+      success: function(res){
+        console.log(res);
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
